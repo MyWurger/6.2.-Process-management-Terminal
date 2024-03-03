4de6251bb348d707a5482f5a374d1636477a8ba6
MyWurger <wdgwdg9@gmail.com>
2024-03-03 03:20:09 +0300

    Реализация терминала с созданием процессов-зомби, то есть без ожидания отработки дочерних процессов родительским

---
+++ build/CMakeFiles/TERMINAL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TERMINAL.dir/main.c.o"
+  "TERMINAL"
+  "TERMINAL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TERMINAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dmitru/Проекты VisualCode/Terminal
+# Build directory: /home/dmitru/Проекты VisualCode/Terminal/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
